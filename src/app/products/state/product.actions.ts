e2b27a1d8c82f3a861a39c00eb7054b3e5256d18
Annie Huang <annie.hn@gmail.com>
2019-01-13 12:52:45 +1100

    Create remaining actions in product.actions

--- src/app/products/state/product.actions.ts
+++ src/app/products/state/product.actions.ts
@@ -1,7 +1,11 @@
 import {Action} from "@ngrx/store";
+import {Product} from "../../../../APM-Demo2/src/app/products/product";
 
 export enum ProductActionTypes {
-  ToggleProductCode = '[Product] Toggle Product Code'
+  ToggleProductCode = '[Product] Toggle Product Code',
+  SetCurrentProduct = '[Product] Set Current Product',
+  ClearCurrentProduct = '[Product] Clear Current Product',
+  InitializeCurrentProduct = '[Product] Initialize Current Product',
 }
 
 export class ToggleProductCode implements Action{
@@ -10,4 +14,27 @@ export class ToggleProductCode implements Action{
   constructor(public payload: boolean) { }
 }
 
-export type ProductActions = ToggleProductCode;
+export class SetCurrentProduct implements Action {
+  readonly type = ProductActionTypes.SetCurrentProduct;
+
+  constructor(public payload: Product) { }
+}
+
+export class ClearCurrentProduct implements Action {
+  readonly type = ProductActionTypes.ClearCurrentProduct;
+
+  // // Don't need it because typescript provides a empty constructor by default
+  // constructor() { }
+}
+
+export class InitializeCurrentProduct implements Action {
+  readonly type = ProductActionTypes.InitializeCurrentProduct;
+
+  // // Don't need it because typescript provides a empty constructor by default
+  // constructor() { }
+}
+
+export type ProductActions = ToggleProductCode
+  | SetCurrentProduct
+  | ClearCurrentProduct
+  | InitializeCurrentProduct;
