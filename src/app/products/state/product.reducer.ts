7d1fcb83d73e0cc00535b30303d86254a22b7ec6
Annie Huang <annie.hn@gmail.com>
2019-01-13 13:15:57 +1100

    Add action type for ClearCurrentProduct and InitializeCurrentProduct through create action.

--- src/app/products/state/product.reducer.ts
+++ src/app/products/state/product.reducer.ts
@@ -71,6 +71,24 @@ export function reducer(state = initialState, action: ProductActions): ProductSt
         currentProduct: { ...action.payload }
       };
 
+    case ProductActionTypes.ClearCurrentProduct:
+      return {
+        ...state,
+        currentProduct: null
+      };
+
+    case ProductActionTypes.InitializeCurrentProduct:
+      return {
+        ...state,
+        currentProduct: {
+          id: 0,
+          productName: '',
+          productCode: 'New',
+          description: '',
+          starRating: 0
+        }
+      };
+      
     default:
       return state;
   }
