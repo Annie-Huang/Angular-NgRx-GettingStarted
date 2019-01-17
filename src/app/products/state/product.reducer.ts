315a471dd646e11d837100c5777dcc98971ae0a1
Annie Huang <annie.hn@gmail.com>
2019-01-17 21:59:16 +1100

    Create reducer to handle UpdateProductSuccess and UpdateProductFail.

--- src/app/products/state/product.reducer.ts
+++ src/app/products/state/product.reducer.ts
@@ -148,6 +148,22 @@ export function reducer(state = initialState, action: ProductActions): ProductSt
         error: action.payload
       };
 
+    case ProductActionTypes.UpdateProductSuccess:
+      // The map() method creates a new array with the results of calling a provided function on every element in the calling array.
+      const updatedProducts = state.products.map(item => action.payload.id === item.id ? action.payload : item);
+      return {
+        ...state,
+        products: updatedProducts,
+        currentProductId: action.payload.id,
+        error: ''
+      };
+
+    case ProductActionTypes.UpdateProductFail:
+      return {
+        ...state,
+        error: action.payload
+      };
+
     default:
       return state;
   }
