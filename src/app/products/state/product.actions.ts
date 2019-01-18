61c26f7e2466476cd52136f4ee8d5bfcf71aa977
Annie Huang <annie.hn@gmail.com>
2019-01-19 10:03:28 +1100

    Homework: Add actions related to DeleteProduct

--- src/app/products/state/product.actions.ts
+++ src/app/products/state/product.actions.ts
@@ -38,7 +38,10 @@ export enum ProductActionTypes {
   UpdateProductFail = '[Product] Update Product Fail',
   CreateProduct = '[Product] Create Product',
   CreateProductSuccess = '[Product] Create Product Success',
-  CreateProductFail = '[Product] Create Product Fail'
+  CreateProductFail = '[Product] Create Product Fail',
+  DeleteProduct = '[Product] Delete Product',
+  DeleteProductSuccess = '[Product] Delete Product Success',
+  DeleteProductFail = '[Product] Delete Product Fail'
 }
 
 // Action Creators
@@ -120,6 +123,25 @@ export class CreateProductFail implements Action {
   constructor(public payload: string) { }
 }
 
+export class DeleteProduct implements Action {
+  readonly type = ProductActionTypes.DeleteProduct;
+
+  constructor(public payload: number) { }
+}
+
+export class DeleteProductSuccess implements Action {
+  readonly type = ProductActionTypes.DeleteProductSuccess;
+
+  constructor(public payload: number) { }
+}
+
+export class DeleteProductFail implements Action {
+  readonly type = ProductActionTypes.DeleteProductFail;
+
+  constructor(public payload: string) { }
+}
+
+// Union the valid types
 export type ProductActions = ToggleProductCode
   | SetCurrentProduct
   | ClearCurrentProduct
@@ -132,5 +154,8 @@ export type ProductActions = ToggleProductCode
   | UpdateProductFail
   | CreateProduct
   | CreateProductSuccess
-  | CreateProductFail;
+  | CreateProductFail
+  | DeleteProduct
+  | DeleteProductSuccess
+  | DeleteProductFail;
 
