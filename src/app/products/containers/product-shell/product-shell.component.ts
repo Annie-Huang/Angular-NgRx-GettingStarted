eec9acddc3db79748631797318f1aae3168623df
Annie Huang <annie.hn@gmail.com>
2019-01-19 11:55:43 +1100

    Make all the input properties that need to use within product-list.component as observable

--- src/app/products/containers/product-shell/product-shell.component.ts
+++ src/app/products/containers/product-shell/product-shell.component.ts
@@ -14,80 +14,90 @@ import * as productActions from '../../state/product.actions';
 @Component({
     templateUrl: './product-shell.component.html'
 })
-export class ProductShellComponent implements OnInit, OnDestroy {
-  errorMessage$: Observable<string>;
+export class ProductShellComponent implements OnInit {
+  // errorMessage$: Observable<string>;
+  // products$: Observable<Product[]>;
+  // componentActive = true;
+  // // errorMessage: string;
+  // displayCode: boolean;
+  // products: Product[];
+  //
+  // // Used to highlight the selected product in the list
+  // selectedProduct: Product | null;
+  // // sub: Subscription;
+
+  displayCode$: Observable<boolean>;
+  selectedProduct$: Observable<Product>;
   products$: Observable<Product[]>;
-  componentActive = true;
-  // errorMessage: string;
+  errorMessage$: Observable<string>;
 
-  displayCode: boolean;
-
-  products: Product[];
-
-  // Used to highlight the selected product in the list
-  selectedProduct: Product | null;
-  // sub: Subscription;
 
   constructor(private store: Store<fromProduct.State>, private productService: ProductService) { }
 
   ngOnInit(): void {
-    // this.sub = this.productService.selectedProductChanges$.subscribe(
-    //   selectedProduct => this.selectedProduct = selectedProduct
-    // );
-    /*
-        this.store.pipe(select(fromProduct.getCurrentProduct)).subscribe(
-          currentProduct => this.selectedProduct = currentProduct
-        );
-    */
-    // Done: Unsubscribe
-    this.store.pipe(select(fromProduct.getCurrentProduct), takeWhile(() => this.componentActive)).subscribe(
-      currentProduct => this.selectedProduct = currentProduct
-    );
-
-    this.errorMessage$ = this.store.pipe(select(fromProduct.getError));
-
-
-    // this.productService.getProducts().subscribe(
-    //   (products: Product[]) => this.products = products,
-    //   (err: any) => this.errorMessage = err.error
-    // );
     this.store.dispatch(new productActions.Load());
+    this.products$ = this.store.pipe(select(fromProduct.getProducts));
+    this.errorMessage$ = this.store.pipe(select(fromProduct.getError));
+    this.selectedProduct$ = this.store.pipe(select(fromProduct.getCurrentProduct));
+    this.displayCode$ = this.store.pipe(select(fromProduct.getShowProductCode));
 
-    // this.store.pipe(select(fromProduct.getProducts))
-    //   .subscribe((products: Product[]) => this.products = products);
-    /*    // !!! Important: Keep in mind that is not subscribe the check the change on the obersable, this is not remove the products from the store.
-        this.store.pipe(select(fromProduct.getProducts), takeWhile(() => this.componentActive))   // Check out the takeFirst and takeUntil method.
-          .subscribe((products: Product[]) => this.products = products);*/
-    // this.products$ = this.store.pipe(select(fromProduct.getProducts));
-    this.products$ = this.store.pipe(select(fromProduct.getProducts)) as Observable<Product[]>;
-
-
-    // this.store.pipe(select('products')).subscribe(
-    //   products => {
-    //     // Need to make sure products slice from state exists first.
-    //     if (products) {
-    //       this.displayCode = products.showProductCode;
-    //     }
-    //   });
-    /*
-        // Don't need to check the if condition (see above) because initialize state was set.
-        this.store.pipe(select('products')).subscribe(
-          products => this.displayCode = products.showProductCode
-          );
-    */
-    // this.store.pipe(select(fromProduct.getShowProductCode)).subscribe(
+    // // this.sub = this.productService.selectedProductChanges$.subscribe(
+    // //   selectedProduct => this.selectedProduct = selectedProduct
+    // // );
+    // /*
+    //     this.store.pipe(select(fromProduct.getCurrentProduct)).subscribe(
+    //       currentProduct => this.selectedProduct = currentProduct
+    //     );
+    // */
+    // // Done: Unsubscribe
+    // this.store.pipe(select(fromProduct.getCurrentProduct), takeWhile(() => this.componentActive)).subscribe(
+    //   currentProduct => this.selectedProduct = currentProduct
+    // );
+    //
+    // this.errorMessage$ = this.store.pipe(select(fromProduct.getError));
+    //
+    //
+    // // this.productService.getProducts().subscribe(
+    // //   (products: Product[]) => this.products = products,
+    // //   (err: any) => this.errorMessage = err.error
+    // // );
+    // this.store.dispatch(new productActions.Load());
+    //
+    // // this.store.pipe(select(fromProduct.getProducts))
+    // //   .subscribe((products: Product[]) => this.products = products);
+    // /*    // !!! Important: Keep in mind that is not subscribe the check the change on the obersable, this is not remove the products from the store.
+    //     this.store.pipe(select(fromProduct.getProducts), takeWhile(() => this.componentActive))   // Check out the takeFirst and takeUntil method.
+    //       .subscribe((products: Product[]) => this.products = products);*/
+    // // this.products$ = this.store.pipe(select(fromProduct.getProducts));
+    // this.products$ = this.store.pipe(select(fromProduct.getProducts)) as Observable<Product[]>;
+    //
+    //
+    // // this.store.pipe(select('products')).subscribe(
+    // //   products => {
+    // //     // Need to make sure products slice from state exists first.
+    // //     if (products) {
+    // //       this.displayCode = products.showProductCode;
+    // //     }
+    // //   });
+    // /*
+    //     // Don't need to check the if condition (see above) because initialize state was set.
+    //     this.store.pipe(select('products')).subscribe(
+    //       products => this.displayCode = products.showProductCode
+    //       );
+    // */
+    // // this.store.pipe(select(fromProduct.getShowProductCode)).subscribe(
+    // //   showProductCode => this.displayCode = showProductCode
+    // // );
+    // // Done: Unsubscribe
+    // this.store.pipe(select(fromProduct.getShowProductCode), takeWhile(() => this.componentActive)).subscribe(
     //   showProductCode => this.displayCode = showProductCode
     // );
-    // Done: Unsubscribe
-    this.store.pipe(select(fromProduct.getShowProductCode), takeWhile(() => this.componentActive)).subscribe(
-      showProductCode => this.displayCode = showProductCode
-    );
   }
 
-  ngOnDestroy(): void {
-    // this.sub.unsubscribe();
-    this.componentActive = false;
-  }
+  // ngOnDestroy(): void {
+  //   // this.sub.unsubscribe();
+  //   this.componentActive = false;
+  // }
 
   checkChanged(value: boolean): void {
     // this.displayCode = value;
